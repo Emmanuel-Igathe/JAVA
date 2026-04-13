bada6abdcd64819c7c8f779e20fd251e7208e2d9
Emmanuel Igathe <emmanueligathe4@gmail.com>
2026-04-13 11:06:03 +0300

    Add files via upload

--- userInput.java
+++ userInput.java
@@ -1,41 +1,18 @@
 import java.util.Scanner;
-// To use the input function in Java we need the Scanner so we import from utility
-
-public class userInput {
-    public static void main(String[] args){
-// To accept user input we have to create a scanner object
-         Scanner scanner = new Scanner(System.in);
-
-         System.out.println("Enter your name: "); //Using println prints on the nextline
-         String name = scanner.nextLine();
-
-
-        System.out.print("Enter your age: "); // Using print() we place input on same line
-        int age = scanner.nextInt();
-
-        // For a double we use the scanner.nextDouble();
-        System.out.print("Enter your gpa: ");
-        double gpa = scanner.nextDouble();
-
-        // For a boolean use scanner.nextBoolean();
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
-
-
-         scanner.close(); //close your scanner to prevent unexpected issues
-        // when you open something you will need to close it at the end
-
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old");
-        System.out.println("Your final gpa is " + gpa);
-
-        if (isStudent){
-            System.out.println("You are enrolled!");
-        }
-        else {
-            System.out.println("You are not enrolled");
-        }
-
-
+public class userInput
+{
+    public static void main(String[]args)
+    {
+        String name;
+        int age;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Key in your name: ");
+        name=input.nextLine();
+        System.out.print("Key in your age: ");
+        age = input.nextInt();
+        System.out.println("\n--User Details----");
+        System.out.println("Name is:" + name);
+        System.out.println("Age is:" + age);
+        input.close();
     }
 }
